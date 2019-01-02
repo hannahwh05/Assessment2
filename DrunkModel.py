f71772fe655b8d151f7fb0eda4ba3494648345e2
Hannah Wheldon <43998956+hannahwh05@users.noreply.github.com>
2019-01-02 12:32:03 +0000

    update

--- DrunkModel.py
+++ DrunkModel.py
@@ -9,6 +9,10 @@ Version 1.0.0
 
 *****what the model does*****
 
+1. Import libraries
+2. Read in environment file "drink.txt"
+3. 
+
 n.b. to run model with random coordinates instead of scraping from webpage, 
 remove 3rd and 4th arguements i.e. y, x in Step 4.
 
@@ -19,11 +23,23 @@ remove 3rd and 4th arguements i.e. y, x in Step 4.
 ###############################################################################
 ################################# Import ######################################
 ###############################################################################
-'''import libraries/functions/packages at top of code'''
+'''import libraries/functions/packages'''
 
 import csv
+
+import drunkframework
+import matplotlib
 #2D plotting library
 import matplotlib.pyplot as plot
+#animate plot
+import matplotlib.animation as ani
+#framework to build tkinter graphics interface
+import tkinter
+matplotlib.use('TkAgg')
+#tkinter backend
+import matplotlib.backends.backend_tkagg
+
+num_of_drunks = 25
 
 ###############################################################################
 #######################'''Step ?: Import environment'''########################
@@ -50,3 +66,47 @@ f.close()
 #test to see environment alone
 plot.imshow(environment)
 plot.show()
+
+###############################################################################
+##########################'''Step 4: Plot agents'''############################
+###############################################################################
+
+drunks = []
+
+for i in range(num_of_drunks):
+    drunks.append(drunkframework.Drunk(environment, drunks))
+'''    
+# set up figure size and axes
+fig = matplotlib.pyplot.figure(figsize=(8, 8))
+ax = fig.add_axes([0, 0, 1, 1])
+
+carry_on = True
+
+def update(frame_number):
+    """
+    Updates the display in the animation:
+    Plot axes based on size of environment.
+    Agents move, eat, share with neighbours and vomit.
+    """
+    #clear previous display           
+    fig.clear()
+    #create global variable to modify local variable outside of function
+    
+    global carry_on
+  
+    #make plot based on size of environment
+    plot.xlim(0, len(environment))
+    plot.ylim(0, len(environment[0]))
+    
+    #for each agent - move, eat, share with neighbours and vomit 
+    for i in range(num_of_drunks):
+        #agents randomly move around environment
+        drunks[i].move()
+
+    #plot environment and set minimum and maximum limits based on environment
+    # values to prevent scaling of colour when model runs
+    #plot.imshow(environment, vmin = 92, vmax= 255)
+        
+    for i in range(num_of_drunks):
+        plot.scatter(drunks[i]._x, drunks[i]._y) 
+'''
